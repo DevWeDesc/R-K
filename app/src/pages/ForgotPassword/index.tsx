e6284781dabd17f7709c4e66a7b9bd1673e3bf3d
ViniHGV <ixxvinicius@gmail.com>
2024-03-26 12:38:43 -0300

    feat: adicionando formulários para as funcionalidades de esqueceu a senha e adicionando cookies para não perder a página e ter que refazer passos já feitos

--- app/src/pages/ForgotPassword/index.tsx
+++ app/src/pages/ForgotPassword/index.tsx
@@ -1,9 +1,40 @@
 import { Header } from "@/components/Header";
+import { useEffect, useState } from "react";
+import { ICardDisplayableInForgotPassword } from "@/@interfaces/ForgotPasword/ICardDisplayableInForgotPassword";
+import { FormEditPassword } from "./FormsForgetPassword/FormEditPassword";
+import { FormSendCodeEmail } from "./FormsForgetPassword/FormSendCodeEmail";
+import { FormVerifyCode } from "./FormsForgetPassword/FormVerifyCode";
+import Cookies from "js-cookie";
 
 export const ForgotPassword = () => {
+  const [cardExible, setCardExible] = useState({
+    sendCodeByEmail: true,
+  } as ICardDisplayableInForgotPassword);
+
   return (
-    <div>
-      <Header />
-    </div>
+    <>
+      <Header navIsVisible={false} />
+      <div className="h-[88dvh] flex justify-center items-center">
+        {Cookies.get("forgotPasswordPage") === "sendCodeByEmail" && (
+          <FormSendCodeEmail
+            cardExible={cardExible}
+            setCardExible={(ev) => setCardExible(ev)}
+          />
+        )}
+        {Cookies.get("forgotPasswordPage") === "VerifyCode" && (
+          <FormVerifyCode
+            cardExible={cardExible}
+            setCardExible={(ev) => setCardExible(ev)}
+          />
+        )}
+
+        {Cookies.get("forgotPasswordPage") === "EditPassword" && (
+          <FormEditPassword
+            cardExible={cardExible}
+            setCardExible={(ev) => setCardExible(ev)}
+          />
+        )}
+      </div>
+    </>
   );
 };
