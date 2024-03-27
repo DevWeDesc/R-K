0e82db674020dd39e2e8c141d0416280ea48e62a
ViniHGV <ixxvinicius@gmail.com>
2024-03-27 12:17:24 -0300

    feat: adicionando a rota de administração de exames

--- app/src/Router.tsx
+++ app/src/Router.tsx
@@ -3,6 +3,8 @@ import { Login } from "./pages/Login";
 import { Register } from "./pages/Register";
 import { ForgotPassword } from "./pages/ForgotPassword";
 import { Home } from "./pages/Home";
+import { AdminExams } from "./pages/AdminExams";
+import { ContextSendEmailClientProvider } from "./contexts/ContextSendEmailClient";
 
 export const Router = () => {
   return (
@@ -12,6 +14,7 @@ export const Router = () => {
         <Route path="register" element={<Register />} />
         <Route path="forgotpassword" element={<ForgotPassword />} />
         <Route path="home" element={<Home />} />
+        <Route path="adminexams" element={<AdminExams />} />
       </Routes>
     </BrowserRouter>
   );
