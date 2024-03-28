0eb2985dd6c45607459392c9392279e837b3e0bf
ViniHGV <ixxvinicius@gmail.com>
2024-03-28 13:47:05 -0300

    feat: adicionando mais um tipo de erro no componente de InputRequired Error

--- app/src/components/Errors/InputRequiredError.tsx
+++ app/src/components/Errors/InputRequiredError.tsx
@@ -4,16 +4,19 @@ import { cva } from "class-variance-authority";
 interface IInputRequiredError {
   inputName: string;
   className?: string;
+  typeError: string;
 }
 const errorVariant = cva("text-center text-sm text-red-800 font-medium");
 
 export const InputRequiredError = ({
   inputName,
   className,
+  typeError,
 }: IInputRequiredError) => {
   return (
     <span className={cn(errorVariant({ className }))}>
-      O campo {inputName} é obrigatório!
+      {typeError === "required" && `O campo ${inputName} é obrigatório!`}
+      {typeError === "isNotValid" && `O campo ${inputName} é inválido!`}
     </span>
   );
 };
