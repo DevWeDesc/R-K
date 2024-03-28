7b02e3af1820cf1196778c66f599e5f82d5bd7ca
ViniHGV <ixxvinicius@gmail.com>
2024-03-28 13:45:24 -0300

    feat: criando interface do componente GenericTable

---
+++ app/src/@interfaces/IGenericTable.ts
@@ -0,0 +1,7 @@
+import { additionalFieldsTableGenericEnum } from "@/enums/additionalFieldsTableGenericEnum";
+import { IListExams } from "./IExams";
+
+export interface IGenericTable {
+  data: IListExams;
+  additionalFields: additionalFieldsTableGenericEnum;
+}
