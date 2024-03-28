2a33221a5af1fb925fd3dcb21dbb9ece24dcf0fb
ViniHGV <ixxvinicius@gmail.com>
2024-03-28 13:49:19 -0300

    fix: corrigindo liks do mock

--- app/src/mocks/CardHomeMock.ts
+++ app/src/mocks/CardHomeMock.ts
@@ -5,7 +5,7 @@ export const CardHomeMock: ICardHome[] = [
     title: "Solicitar Exames",
     description:
       "Descubra uma variedade de exames veterinários disponíveis para ajudar a manter a saúde e o bem-estar dos seus animais de estimação. Desde exames de rotina até procedimentos mais especializados, estamos aqui para oferecer suporte completo.",
-    LinkButton: "/solicitedexams",
+    LinkButton: "/exams/available",
     TextButton: "Visualizar Exames Disponíveis",
     OpenModal: true,
   },
@@ -13,7 +13,7 @@ export const CardHomeMock: ICardHome[] = [
     title: "Cadastrar Exames",
     description:
       "Descubra uma variedade de exames veterinários disponíveis para ajudar a manter a saúde e o bem-estar dos seus animais de estimação. Desde exames de rotina até procedimentos mais especializados, estamos aqui para oferecer suporte completo.",
-    LinkButton: "/adminexams",
+    LinkButton: "/exams/admin",
     TextButton: "Cadastrar Exames",
     Role: "admin",
   },
