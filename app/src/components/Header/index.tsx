0c06539d1ec6971d6e76145ca8cba912ae810748
ViniHGV <ixxvinicius@gmail.com>
2024-03-28 13:48:00 -0300

    feat: adicionando navegabilidade ao clicar na logo do Header

--- app/src/components/Header/index.tsx
+++ app/src/components/Header/index.tsx
@@ -1,18 +1,25 @@
 import SoonWhite from "@/../public/assets/LogoR&KWhite.png";
 import { Nav } from "./Nav";
+import { useNavigate } from "react-router-dom";
 
 interface IHeader {
   navIsVisible: boolean;
 }
 
 export const Header = ({ navIsVisible }: IHeader) => {
+  const navigate = useNavigate();
   return (
     <div
       className={`bg-redDefault flex items-center px-8 py-2 ${
         !navIsVisible ? "justify-center" : "justify-between"
       }`}
     >
-      <img src={SoonWhite} className="max-h-20" alt="" />
+      <img
+        onClick={() => navigate("/home")}
+        src={SoonWhite}
+        className="max-h-20 cursor-pointer"
+        alt=""
+      />
       {navIsVisible && <Nav />}
     </div>
   );
