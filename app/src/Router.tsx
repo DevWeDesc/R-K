9bcbab551f8ef5cc20f91a153635588aa2bc353b
ViniHGV <ixxvinicius@gmail.com>
2024-04-02 12:42:57 -0300

    feat: adicionando páginas de finalização de guia e relatórios

--- app/src/Router.tsx
+++ app/src/Router.tsx
@@ -4,8 +4,10 @@ import { Register } from "./pages/Register";
 import { ForgotPassword } from "./pages/ForgotPassword";
 import { Home } from "./pages/Home";
 import { AdminExams } from "./pages/AdminExams";
-import { ExamsAvailable } from "./pages/ExamsAvailable/indext";
-import { TabPreview } from "./pages/TabPreview";
+import { ExamsAvailable } from "./pages/ExamsAvailable";
+import { GuidePreview } from "./pages/GuidePreview";
+import { Reports } from "./pages/Reports";
+import { FillingInTheGuide } from "./pages/FillingInTheGuide";
 
 export const Router = () => {
   return (
@@ -17,7 +19,9 @@ export const Router = () => {
         <Route path="home" element={<Home />} />
         <Route path="exams/admin" element={<AdminExams />} />
         <Route path="exams/available" element={<ExamsAvailable />} />
-        <Route path="tab/preview" element={<TabPreview />} />
+        <Route path="guide/preview" element={<GuidePreview />} />
+        <Route path="guide/fill" element={<FillingInTheGuide />} />
+        <Route path="reports" element={<Reports />} />
       </Routes>
     </BrowserRouter>
   );
