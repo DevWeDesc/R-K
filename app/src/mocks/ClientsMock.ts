13bc89240148793501e293f6a70b827bcaab6ccc
ViniHGV <ixxvinicius@gmail.com>
2024-04-03 11:58:39 -0300

    feat: adicionando pets no mock de Clientes

--- app/src/mocks/ClientsMock.ts
+++ app/src/mocks/ClientsMock.ts
@@ -2,6 +2,7 @@ export interface IClient {
   email: string;
   name: string;
   phone: string;
+  pets?: string[];
 }
 
 export const ClientsMock: IClient[] = [
@@ -9,10 +10,12 @@ export const ClientsMock: IClient[] = [
     email: "cliente@gmail.com",
     name: "cliente teste",
     phone: "(11) 99999-9999",
+    pets: ["Luna", "Mel"],
   },
   {
     email: "danilo@gmail.com",
     name: "Danilo Scalia",
     phone: "(11) 91111-1111",
+    pets: ["Luna", "Mel"],
   },
 ];
