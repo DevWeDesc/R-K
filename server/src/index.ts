a39fa078697c1ca971cfbc02ee29e880f9c03043
ViniHGV <ixxvinicius@gmail.com>
2024-04-10 12:44:00 -0300

    feat: criando Nova instâncias de objetos de useCase

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,3 +1,5 @@
+import CreateClientUseCase from "./domain/useCases/Clients/CreateClientUseCase";
+import GetAllClientsUseCase from "./domain/useCases/Clients/GetAllClientsUseCase";
 import CreateExamsUseCase from "./domain/useCases/Exams/CreateExamsUseCase";
 import GetExamsUseCase from "./domain/useCases/Exams/GetExamsUseCase";
 import CreateGroupUseCase from "./domain/useCases/Groups/CreateGroupUseCase";
@@ -8,19 +10,18 @@ import { GetAllLoginsUseCase } from "./domain/useCases/UsersLogin/GetAllUsersUse
 import { GetUniqueUserUseCase } from "./domain/useCases/UsersLogin/GetUniqueUserUseCase";
 import GetAllVeterinariansUseCase from "./domain/useCases/Veterinarians/GetAllVeterinariansUseCase";
 import GetUniqueVeterinarianUseCase from "./domain/useCases/Veterinarians/GetUniqueVeterinarianUseCase";
+import ClientsRepositories from "./infra/repositories/Clients/ClientsRepositories";
 import ExamsRepository from "./infra/repositories/Exams/ExamsRepository";
 import GroupRepository from "./infra/repositories/Groups/GroupRepository";
 import { UserLoginRepository } from "./infra/repositories/UserLogin/UserLoginRepository";
 import { VeterinarianRepository } from "./infra/repositories/Veterinarian/VeterinarianRepository";
-
-// Errors
-export const VeterinarianNotFound = require("./domain/errors/VeterinarianError/VeterinarianNotFound");
 
 // Repositories
 export const userLoginRepository = new UserLoginRepository();
 export const veterinarianRepository = new VeterinarianRepository();
 export const examsRepository = new ExamsRepository();
 export const groupRepository = new GroupRepository();
+export const clientsRepositories = new ClientsRepositories();
 
 // Use Cases
 export const createLoginUseCase = new CreateLoginUseCase(
@@ -43,3 +44,7 @@ export const getExamsUseCase = new GetExamsUseCase(examsRepository);
 export const createExamsUseCase = new CreateExamsUseCase(examsRepository);
 export const createGroupUseCase = new CreateGroupUseCase(groupRepository);
 export const getGroupsUseCase = new GetGroupsUseCase(groupRepository);
+export const getAllClientsUseCase = new GetAllClientsUseCase(
+  clientsRepositories
+);
+export const createClientUseCase = new CreateClientUseCase(clientsRepositories);
