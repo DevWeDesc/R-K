7fe99a8037f3a06f6980582726389cbc4c48fea7
ViniHGV <ixxvinicius@gmail.com>
2024-04-11 11:25:06 -0300

    feat: criando teste para a funcionalidade de criação de cliente

---
+++ server/src/domain/useCases/Customers/CreateCustomer/CreateCustomer.spec.ts
@@ -0,0 +1,56 @@
+import { createCustomerUseCase } from "../../../..";
+import { CustomerRequestDTO } from "../../../../application/DTOs/CustomersDTO/CustomersRequestDTO";
+import EmailAlreadyUsedError from "../../../errors/Customers/EmailAlreadyUsedError";
+import EmailNotValidError from "../../../errors/Customers/EmailNotValidError";
+import PhoneAlreadyUsedError from "../../../errors/Customers/PhoneAlreadyUsedError";
+
+describe("Create Customer Tests", () => {
+  test("Checks if the email is not valid", async () => {
+    /*
+     *Se o email não tiver o formato valido como por
+     *exemplo emailvalido@example.com o erro EmailNotValid é dispardo
+     *fazendo a validação do cadastro
+     */
+    const data: CustomerRequestDTO = {
+      name: "EmailIsValid",
+      email: "vinicius@example",
+      phone: "(11) 99999-9999",
+    };
+
+    await expect(createCustomerUseCase.execute(data)).rejects.toThrow(
+      EmailNotValidError
+    );
+  });
+
+  test("Check existing email", async () => {
+    /*
+     *Se o email já estiver cadastrado no sistema
+     *o erro EmailAlreadyUSed é disparado fazendo a validação do cadastro
+     */
+    const data: CustomerRequestDTO = {
+      name: "ExistEmail",
+      email: "vinicius@gmail.com",
+      phone: "(11) 99999-9999",
+    };
+
+    await expect(createCustomerUseCase.execute(data)).rejects.toThrow(
+      EmailAlreadyUsedError
+    );
+  });
+
+  test("Check existing phone", async () => {
+    /*
+     *Se o telefone já estiver cadastrado no sistema
+     *o erro PhoneAlreadyUsed é disparado fazendo a validação do cadastro
+     */
+    const data: CustomerRequestDTO = {
+      name: "Exists Phone",
+      email: "existsPhone@gmail.com",
+      phone: "(11) 99999-9999",
+    };
+
+    await expect(createCustomerUseCase.execute(data)).rejects.toThrow(
+      PhoneAlreadyUsedError
+    );
+  });
+});
