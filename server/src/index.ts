abc5bc41541e0d7d382e7383d7897b2cc607303d
ViniHGV <ixxvinicius@gmail.com>
2024-04-11 11:20:13 -0300

    fix: fazendo novas importações e criando novas instâncias

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,5 +1,6 @@
-import CreateClientUseCase from "./domain/useCases/Clients/CreateClientUseCase";
-import GetAllClientsUseCase from "./domain/useCases/Clients/GetAllClientsUseCase";
+import CreateCustomerUseCase from "./domain/useCases/Customers/CreateCustomer/CreateCustomerUseCase";
+import GetAllCustomersUseCase from "./domain/useCases/Customers/GetAllCustomer/GetAllCustomersUseCase";
+import GetUniqueCustomerUseCase from "./domain/useCases/Customers/GetUniqueCustomer/GetUniqueUserUseCase";
 import CreateExamsUseCase from "./domain/useCases/Exams/CreateExamsUseCase";
 import GetExamsUseCase from "./domain/useCases/Exams/GetExamsUseCase";
 import CreateGroupUseCase from "./domain/useCases/Groups/CreateGroupUseCase";
@@ -10,7 +11,7 @@ import { GetAllLoginsUseCase } from "./domain/useCases/UsersLogin/GetAllUsersUse
 import { GetUniqueUserUseCase } from "./domain/useCases/UsersLogin/GetUniqueUserUseCase";
 import GetAllVeterinariansUseCase from "./domain/useCases/Veterinarians/GetAllVeterinariansUseCase";
 import GetUniqueVeterinarianUseCase from "./domain/useCases/Veterinarians/GetUniqueVeterinarianUseCase";
-import ClientsRepositories from "./infra/repositories/Clients/ClientsRepositories";
+import CustomerRepository from "./infra/repositories/Customers/CustomerRepository";
 import ExamsRepository from "./infra/repositories/Exams/ExamsRepository";
 import GroupRepository from "./infra/repositories/Groups/GroupRepository";
 import { UserLoginRepository } from "./infra/repositories/UserLogin/UserLoginRepository";
@@ -21,7 +22,7 @@ export const userLoginRepository = new UserLoginRepository();
 export const veterinarianRepository = new VeterinarianRepository();
 export const examsRepository = new ExamsRepository();
 export const groupRepository = new GroupRepository();
-export const clientsRepositories = new ClientsRepositories();
+export const customerRepositories = new CustomerRepository();
 
 // Use Cases
 export const createLoginUseCase = new CreateLoginUseCase(
@@ -44,7 +45,13 @@ export const getExamsUseCase = new GetExamsUseCase(examsRepository);
 export const createExamsUseCase = new CreateExamsUseCase(examsRepository);
 export const createGroupUseCase = new CreateGroupUseCase(groupRepository);
 export const getGroupsUseCase = new GetGroupsUseCase(groupRepository);
-export const getAllClientsUseCase = new GetAllClientsUseCase(
-  clientsRepositories
+export const createCustomerUseCase = new CreateCustomerUseCase(
+  customerRepositories
 );
-export const createClientUseCase = new CreateClientUseCase(clientsRepositories);
+export const getUniqueCustomerUseCase = new GetUniqueCustomerUseCase(
+  customerRepositories
+);
+export const getAllCustomersUseCase = new GetAllCustomersUseCase(
+  customerRepositories,
+  getUniqueCustomerUseCase
+);
