720dfb86fc8383c46055fd66dcb9c5025d9c8142
ViniHGV <ixxvinicius@gmail.com>
2024-04-12 13:53:13 -0300

    feat: criando novas instâncias dos objetos

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,10 +1,15 @@
 import CreateCustomerUseCase from "./domain/useCases/Customers/CreateCustomer/CreateCustomerUseCase";
+import DeletedCustomerUseCase from "./domain/useCases/Customers/DeletedCustomer/DeletedCustomerUseCase";
 import GetAllCustomersUseCase from "./domain/useCases/Customers/GetAllCustomer/GetAllCustomersUseCase";
 import GetUniqueCustomerUseCase from "./domain/useCases/Customers/GetUniqueCustomer/GetUniqueUserUseCase";
+import UpdatedCustomerUseCase from "./domain/useCases/Customers/UpdatedCustomer/UpdatedCustomerUseCase";
 import CreateExamsUseCase from "./domain/useCases/Exams/CreateExamsUseCase";
 import GetExamsUseCase from "./domain/useCases/Exams/GetExamsUseCase";
 import CreateGroupUseCase from "./domain/useCases/Groups/CreateGroupUseCase";
 import GetGroupsUseCase from "./domain/useCases/Groups/GetGroupsUseCase";
+import CreatePetUseCase from "./domain/useCases/Pets/CreatePet/CreatePetUseCase";
+import GetAllPetsUseCase from "./domain/useCases/Pets/GetAllPets/GetAllPetsUseCase";
+import GetPetsPerCustomerUseCase from "./domain/useCases/Pets/GetPetsPerCustomer/GetPetsPerCustomerUseCase";
 import AutenticationUserUseCase from "./domain/useCases/UsersLogin/AutenticationUserUseCase";
 import { CreateLoginUseCase } from "./domain/useCases/UsersLogin/CreateLoginUseCase";
 import { GetAllLoginsUseCase } from "./domain/useCases/UsersLogin/GetAllUsersUseCase";
@@ -14,6 +19,7 @@ import GetUniqueVeterinarianUseCase from "./domain/useCases/Veterinarians/GetUni
 import CustomerRepository from "./infra/repositories/Customers/CustomerRepository";
 import ExamsRepository from "./infra/repositories/Exams/ExamsRepository";
 import GroupRepository from "./infra/repositories/Groups/GroupRepository";
+import PetsRepository from "./infra/repositories/Pets/PetsRepository";
 import { UserLoginRepository } from "./infra/repositories/UserLogin/UserLoginRepository";
 import { VeterinarianRepository } from "./infra/repositories/Veterinarian/VeterinarianRepository";
 
@@ -22,7 +28,8 @@ export const userLoginRepository = new UserLoginRepository();
 export const veterinarianRepository = new VeterinarianRepository();
 export const examsRepository = new ExamsRepository();
 export const groupRepository = new GroupRepository();
-export const customerRepositories = new CustomerRepository();
+export const customerRepository = new CustomerRepository();
+export const petsRepository = new PetsRepository();
 
 // Use Cases
 export const createLoginUseCase = new CreateLoginUseCase(
@@ -45,13 +52,34 @@ export const getExamsUseCase = new GetExamsUseCase(examsRepository);
 export const createExamsUseCase = new CreateExamsUseCase(examsRepository);
 export const createGroupUseCase = new CreateGroupUseCase(groupRepository);
 export const getGroupsUseCase = new GetGroupsUseCase(groupRepository);
-export const createCustomerUseCase = new CreateCustomerUseCase(
-  customerRepositories
-);
 export const getUniqueCustomerUseCase = new GetUniqueCustomerUseCase(
-  customerRepositories
+  customerRepository
+);
+export const createCustomerUseCase = new CreateCustomerUseCase(
+  customerRepository,
+  getUniqueCustomerUseCase
 );
 export const getAllCustomersUseCase = new GetAllCustomersUseCase(
-  customerRepositories,
+  customerRepository,
+  getUniqueCustomerUseCase
+);
+
+export const updatedCustomerUseCase = new UpdatedCustomerUseCase(
+  customerRepository,
+  getUniqueCustomerUseCase
+);
+
+export const deletedCustomerUseCase = new DeletedCustomerUseCase(
+  customerRepository,
+  getUniqueCustomerUseCase
+);
+
+export const getAllPetsUseCase = new GetAllPetsUseCase(petsRepository);
+export const createPetUseCase = new CreatePetUseCase(
+  petsRepository,
+  getUniqueCustomerUseCase
+);
+export const getPetsPerCustomerUseCase = new GetPetsPerCustomerUseCase(
+  petsRepository,
   getUniqueCustomerUseCase
 );
