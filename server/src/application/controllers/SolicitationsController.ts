d67adb809b1c515ad5a7eda80edae6e8757a1841
ViniHGV <ixxvinicius@gmail.com>
2024-04-16 10:13:14 -0300

    feat: criando SolicitationsController para lidar com as requisições de solicitações

---
+++ server/src/application/controllers/SolicitationsController.ts
@@ -0,0 +1,43 @@
+import { FastifyRequest, FastifyReply } from "fastify";
+import {
+  createSolicitationsUseCase,
+  getAllSolicitationsUseCase,
+  getUniqueSolicitationsUseCase,
+} from "../..";
+import { SolicitationRequestDTO } from "../DTOs/SolicitationsDTO/SolicitationRequestDTO";
+
+export const SolicitationsController = {
+  GetAllSolicitations: async (request: FastifyRequest, reply: FastifyReply) => {
+    try {
+      const res = await getAllSolicitationsUseCase.execute();
+      return reply.code(200).send(res);
+    } catch (err) {
+      return reply.code(400).send(err);
+    }
+  },
+  GetSolicitationById: async (
+    request: FastifyRequest<{ Params: { id: string } }>,
+    reply: FastifyReply
+  ) => {
+    try {
+      const { id } = request.params;
+      const res = await getUniqueSolicitationsUseCase.execute(id);
+      return reply.code(200).send(res);
+    } catch (err) {
+      return reply.code(400).send(err);
+    }
+  },
+
+  CreateSolicitations: async (
+    request: FastifyRequest<{ Body: SolicitationRequestDTO }>,
+    reply: FastifyReply
+  ) => {
+    try {
+      const { body } = request;
+      const res = await createSolicitationsUseCase.execute(body);
+      return reply.code(200).send(res);
+    } catch (err) {
+      return reply.code(400).send(err);
+    }
+  },
+};
