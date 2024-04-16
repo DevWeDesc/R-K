9c6e98cf4b213130465ce5198d838d4f60a28b3c
ViniHGV <ixxvinicius@gmail.com>
2024-04-16 10:10:13 -0300

    feat: criando função responsável unicamente pelo registro de rotas, deixando o código menos acoplado

---
+++ server/src/application/routes/RegisterRoutes/index.ts
@@ -0,0 +1,20 @@
+import { FastifyInstance } from "fastify";
+import { customersRoutes } from "../CustomerRoutes";
+import { examsInPetOnSolicitationsRoutes } from "../ExamsInPetOnSolicitationsRoutes";
+import { examsRoutes } from "../ExamsRoutes";
+import { groupsRoutes } from "../GroupsRoutes";
+import { usersRoutes } from "../LoginRoutes";
+import { petsRoutes } from "../PetsRoutes";
+import { solicitationsRoutes } from "../SolicitationsRoutes";
+import { veterinarianRoutes } from "../VeterinarianRoutes";
+
+export const registerRoutes = (app: FastifyInstance) => {
+  app.register(usersRoutes);
+  app.register(veterinarianRoutes);
+  app.register(examsRoutes);
+  app.register(groupsRoutes);
+  app.register(customersRoutes);
+  app.register(petsRoutes);
+  app.register(solicitationsRoutes);
+  app.register(examsInPetOnSolicitationsRoutes);
+};
