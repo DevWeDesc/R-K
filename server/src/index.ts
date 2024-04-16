32ddd33c3dc18fd9ff0a16726583b668a71697e0
ViniHGV <ixxvinicius@gmail.com>
2024-04-16 10:04:08 -0300

    feat: criando novas instâncias de casos de uso e repositories

--- server/src/index.ts
+++ server/src/index.ts
@@ -5,11 +5,17 @@ import GetUniqueCustomerUseCase from "./domain/useCases/Customers/GetUniqueCusto
 import UpdatedCustomerUseCase from "./domain/useCases/Customers/UpdatedCustomer/UpdatedCustomerUseCase";
 import CreateExamsUseCase from "./domain/useCases/Exams/CreateExamsUseCase";
 import GetExamsUseCase from "./domain/useCases/Exams/GetExamsUseCase";
+import CreateExamsInPetOnSolicitationsUseCase from "./domain/useCases/ExamsInPetOnSolicitations/CreateExamsInPetOnSolicitationsUseCase";
+import GetAllExamsInPetOnSolicitationsUseCase from "./domain/useCases/ExamsInPetOnSolicitations/GetAllExamsInPetOnSolicitationsUseCase";
 import CreateGroupUseCase from "./domain/useCases/Groups/CreateGroupUseCase";
 import GetGroupsUseCase from "./domain/useCases/Groups/GetGroupsUseCase";
 import CreatePetUseCase from "./domain/useCases/Pets/CreatePet/CreatePetUseCase";
 import GetAllPetsUseCase from "./domain/useCases/Pets/GetAllPets/GetAllPetsUseCase";
 import GetPetsPerCustomerUseCase from "./domain/useCases/Pets/GetPetsPerCustomer/GetPetsPerCustomerUseCase";
+import { AddExamInSolicitationUseCase } from "./domain/useCases/Solicitations/AddExamInSolicitation/AddExamInSolicitationUseCase";
+import CreateSolicitationsUseCase from "./domain/useCases/Solicitations/CreateSolicitations/CreateSolicitationsUseCase";
+import { GetAllSolicitationsUseCase } from "./domain/useCases/Solicitations/GetAllSolicitations/GetAllSolicitationsUseCase";
+import GetUniqueSolicitationsUseCase from "./domain/useCases/Solicitations/GetUniqueSolicitation/GetUniqueSolicitationsUseCase";
 import AutenticationUserUseCase from "./domain/useCases/UsersLogin/AutenticationUserUseCase";
 import { CreateLoginUseCase } from "./domain/useCases/UsersLogin/CreateLoginUseCase";
 import { GetAllLoginsUseCase } from "./domain/useCases/UsersLogin/GetAllUsersUseCase";
@@ -18,8 +24,10 @@ import GetAllVeterinariansUseCase from "./domain/useCases/Veterinarians/GetAllVe
 import GetUniqueVeterinarianUseCase from "./domain/useCases/Veterinarians/GetUniqueVeterinarianUseCase";
 import CustomerRepository from "./infra/repositories/Customers/CustomerRepository";
 import ExamsRepository from "./infra/repositories/Exams/ExamsRepository";
+import ExamsInPetOnSolicitationsRepository from "./infra/repositories/ExamsInPetOnSolicitations/ExamsInPetOnSolicitationsRepository";
 import GroupRepository from "./infra/repositories/Groups/GroupRepository";
 import PetsRepository from "./infra/repositories/Pets/PetsRepository";
+import SolicitationsRepository from "./infra/repositories/Solicitations/SolicitationsRepository";
 import { UserLoginRepository } from "./infra/repositories/UserLogin/UserLoginRepository";
 import { VeterinarianRepository } from "./infra/repositories/Veterinarian/VeterinarianRepository";
 
@@ -30,6 +38,9 @@ export const examsRepository = new ExamsRepository();
 export const groupRepository = new GroupRepository();
 export const customerRepository = new CustomerRepository();
 export const petsRepository = new PetsRepository();
+export const solicitationsRepository = new SolicitationsRepository();
+export const examsInPetOnSolicitationsRepository =
+  new ExamsInPetOnSolicitationsRepository();
 
 // Use Cases
 export const createLoginUseCase = new CreateLoginUseCase(
@@ -82,4 +93,24 @@ export const createPetUseCase = new CreatePetUseCase(
 export const getPetsPerCustomerUseCase = new GetPetsPerCustomerUseCase(
   petsRepository,
   getUniqueCustomerUseCase
+);
+export const getAllSolicitationsUseCase = new GetAllSolicitationsUseCase(
+  solicitationsRepository
+);
+export const createSolicitationsUseCase = new CreateSolicitationsUseCase(
+  solicitationsRepository
+);
+export const addExamInSolicitationUseCase = new AddExamInSolicitationUseCase(
+  examsInPetOnSolicitationsRepository
+);
+export const getAllExamsInPetOnSolicitationsUseCase =
+  new GetAllExamsInPetOnSolicitationsUseCase(
+    examsInPetOnSolicitationsRepository
+  );
+export const createExamsInPetOnSolicitationsUseCase =
+  new CreateExamsInPetOnSolicitationsUseCase(
+    examsInPetOnSolicitationsRepository
+  );
+export const getUniqueSolicitationsUseCase = new GetUniqueSolicitationsUseCase(
+  solicitationsRepository
 );
