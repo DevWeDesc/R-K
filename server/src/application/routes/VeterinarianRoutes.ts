a141d4fecc949d9fbadc9425859508b2f6ea1a26
ViniHGV <ixxvinicius@gmail.com>
2024-04-18 10:33:26 -0300

    feat: criando rota para solicitações por veterinários

--- server/src/application/routes/VeterinarianRoutes.ts
+++ server/src/application/routes/VeterinarianRoutes.ts
@@ -3,4 +3,8 @@ import { VeterinarianController } from "../controllers/VeterinarianController";
 
 export async function veterinarianRoutes(app: FastifyInstance) {
   app.get("/veterinarian", VeterinarianController.GetVeterinarianQuery);
+  app.get(
+    "/veterinarian/solicitations",
+    VeterinarianController.GetQuantitySolicitationsPerVeterinarian
+  );
 }
