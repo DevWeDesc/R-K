4fd9ce234fc0ae7fcc0f7b4c79d3c29ae2658ba6
ViniHGV <ixxvinicius@gmail.com>
2024-04-18 10:32:26 -0300

    feat: adicionando autenticação na rota de listagem de clientes

--- server/src/application/routes/CustomerRoutes.ts
+++ server/src/application/routes/CustomerRoutes.ts
@@ -1,9 +1,16 @@
 import { FastifyInstance } from "fastify";
 import { CustomersController } from "../controllers/CustomersController";
+import { AuthMiddleware } from "../middlewares/Auth";
 
 export async function customersRoutes(app: FastifyInstance) {
   app.post("/customers", CustomersController.CreateCustomer);
-  app.get("/customers", CustomersController.GetAllCustomers);
+  app.get(
+    "/customers",
+    {
+      preHandler: AuthMiddleware,
+    },
+    CustomersController.GetAllCustomers
+  );
   app.get("/customers/:id", CustomersController.GetCustomerById);
   app.put("/customers/:id", CustomersController.UpdatedCustomer);
   app.delete("/customers/:id", CustomersController.DeletedCustomer);
