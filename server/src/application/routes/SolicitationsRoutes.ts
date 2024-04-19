75720cffcd922fa65611e9123a06ff0ceae487fc
ViniHGV <ixxvinicius@gmail.com>
2024-04-19 13:04:21 -0300

    feat: criando rota para finalização de solicitação

--- server/src/application/routes/SolicitationsRoutes.ts
+++ server/src/application/routes/SolicitationsRoutes.ts
@@ -5,7 +5,5 @@ export async function solicitationsRoutes(app: FastifyInstance) {
   app.get("/solicitations", SolicitationsController.GetAllSolicitations);
   app.post("/solicitations", SolicitationsController.CreateSolicitations);
   app.get("/solicitations/:id", SolicitationsController.GetSolicitationById);
-  // app.get("/solicitations/:id", CustomersController.GetCustomerById);
-  // app.put("/solicitations/:id", CustomersController.UpdatedCustomer);
-  // app.delete("/solicitations/:id", CustomersController.DeletedCustomer);
+  app.patch("/solicitations/:id", SolicitationsController.FinalizeSolicitation);
 }
