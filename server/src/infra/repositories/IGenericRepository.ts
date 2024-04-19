91e63b1e8dcc766c3cf422f3548380cae081427b
ViniHGV <ixxvinicius@gmail.com>
2024-04-19 13:10:36 -0300

    fix: atualizando tipagem

--- server/src/infra/repositories/IGenericRepository.ts
+++ server/src/infra/repositories/IGenericRepository.ts
@@ -1,5 +1,5 @@
 export interface IGenericRepository<T, TP> {
-  update(id: number | string, entity: T): Promise<TP>;
+  update(id: number | string, entity: any): Promise<TP>;
   findById(id: number | string): Promise<TP | null>;
   listAll(): Promise<TP[] | null>;
   create(entity: T): Promise<TP>;
