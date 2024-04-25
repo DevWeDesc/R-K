cbe5a6e24a1b5e934592393b12c90a3a60881348
ViniHGV <ixxvinicius@gmail.com>
2024-04-25 09:31:40 -0300

    feat: adicionando endpoints de deleção e edição de exames

--- server/src/application/controllers/ExamsController.ts
+++ server/src/application/controllers/ExamsController.ts
@@ -1,5 +1,10 @@
 import { FastifyRequest, FastifyReply } from "fastify";
-import { createExamsUseCase, getExamsUseCase } from "../..";
+import {
+  createExamsUseCase,
+  deleteExamUseCase,
+  getExamsUseCase,
+  updateExamUseCase,
+} from "../..";
 import { ExamsRequestDTO } from "../DTOs/ExamsDTO/ExamsRequestDTO";
 
 export const ExamsController = {
@@ -30,6 +35,40 @@ export const ExamsController = {
       const res = await createExamsUseCase.execute(body);
 
       return reply.code(201).send(res);
+    } catch (err) {
+      return reply.code(400).send(err);
+    }
+  },
+
+  RemoveExam: async (
+    request: FastifyRequest<{
+      Params: { id: string };
+    }>,
+    reply: FastifyReply
+  ) => {
+    const { id } = request.params;
+    try {
+      await deleteExamUseCase.execute(parseInt(id));
+
+      return reply.code(204).send();
+    } catch (err) {
+      return reply.code(400).send(err);
+    }
+  },
+
+  UpdateExams: async (
+    request: FastifyRequest<{
+      Params: { id: string };
+      Body: ExamsRequestDTO;
+    }>,
+    reply: FastifyReply
+  ) => {
+    try {
+      const { body } = request;
+      const { id } = request.params;
+      await updateExamUseCase.execute(parseInt(id), body);
+
+      return reply.code(204).send();
     } catch (err) {
       return reply.code(400).send(err);
     }
--- server/src/application/routes/ExamsRoutes.ts
+++ server/src/application/routes/ExamsRoutes.ts
@@ -4,4 +4,6 @@ import { ExamsController } from "../controllers/ExamsController";
 export async function examsRoutes(app: FastifyInstance) {
   app.get("/exams", ExamsController.GetExams);
   app.post("/exams", ExamsController.CreateExams);
+  app.put("/exams/:id", ExamsController.UpdateExams);
+  app.delete("/exams/:id", ExamsController.RemoveExam);
 }
