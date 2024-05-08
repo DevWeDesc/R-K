3e5c280c7d62253a0ebac8a40f14779a3f47500b
ViniHGV <ixxvinicius@gmail.com>
2024-05-08 09:52:29 -0300

    fix: adicionando verificador JWT

--- server/src/utils/jwt/VerifyToken.ts
+++ server/src/utils/jwt/VerifyToken.ts
@@ -1,7 +1,7 @@
-import { Secret, verify } from "jsonwebtoken";
+import { JwtPayload, Secret, verify } from "jsonwebtoken";
 require("dotenv").config();
 
-export const VerifyToken = (token: string) => {
+export const VerifyToken = async (token: string): Promise<any> => {
   const decoded = verify(token, process.env.SECRET as Secret);
   return decoded;
 };
