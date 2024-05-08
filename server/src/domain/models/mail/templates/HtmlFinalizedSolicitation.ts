0b0dc692d03f0a9383380a7e5a643b03678d25fa
ViniHGV <ixxvinicius@gmail.com>
2024-05-08 09:46:03 -0300

    fix: ajustando template html de criação de pdf

--- server/src/domain/models/mail/templates/HtmlFinalizedSolicitation.ts
+++ server/src/domain/models/mail/templates/HtmlFinalizedSolicitation.ts
@@ -1,10 +1,11 @@
+import { FormatedDate } from "../../../../utils/FormatedDate";
 import { SolicitationModel } from "../../Solicitation";
 
 export const HtmlFinalizedSolicitation = (
   solicitationDetails: SolicitationModel | null | any
 ) => {
   return `<body
-  style="margin: 0; padding: 0; font-family: Arial, Helvetica, sans-serif; position: relative;"
+  style="margin: 0; padding: 0; font-family: 'Times New Roman', Times, serif; position: relative;"
 >
   <div style="margin: 6.5% 5%">
     <div style="width: 30%; text-align: center; margin: auto;">
@@ -16,41 +17,68 @@ export const HtmlFinalizedSolicitation = (
     </div>
     <br />
     <br />
-    <div style="border: 2px solid black; padding: 20px;">
+    <div style="border: 1px solid gray; padding: 20px;">
       <p style="margin: 0; text-align: center; font-size: 22px; font-weight: bold;"> Solicitação de Exame</p>
       <br>
-      
-      <p style="margin: 5px;"><strong>Nome do Tutor: </strong> ${
-        solicitationDetails.pet.customer.name
-      }</p>
-      <p style="margin: 5px;"><strong>CPF: </strong> ${
-        solicitationDetails.pet.customer.cpf
-      }</p>
-      <p style="margin: 5px;"><strong>E-mail: </strong> ${
-        solicitationDetails.pet.customer.email
-      }</p>
+      <table style="width: 100%;">
+        <tr>
+          <td>  <p style="margin: 0 5px;"><strong>Nome do Tutor: </strong> ${
+            solicitationDetails.pet.customer.name
+          }</p></td>
+          <td style="text-align: end;"><strong>Data e Hora:</strong> ${FormatedDate(
+            new Date(),
+            "short",
+            "medium"
+          ).replace(",", " - ")}</td>
+        </tr>
+        <tr>
+          <td colspan="2">
+            <p style="margin: 0 5px;"><strong>CPF: </strong> ${
+              solicitationDetails.pet.customer.cpf
+            }</p>
+          </td>
+        </tr>
+        <tr>
+          <td colspan="2">
+            <p style="margin: 0 5px;"><strong>E-mail: </strong> ${
+              solicitationDetails.pet.customer.email
+            }</p>
+          </td>
+        </tr>
+      </table>
       <br>
-
-      <p><strong>Dados do Pet:</strong></p>
-      <div style="display: flex; justify-content: space-between; align-items: center;">
-        <p style="margin: 5px;"><strong>Nome do Animal: </strong> ${
-          solicitationDetails.pet.name
-        }</p>
-        <p style="margin: 5px;"><strong>Espécie: </strong> ${
-          solicitationDetails.pet.name
-        }</p>
-      </div>
-      <div style="display: flex; justify-content: space-between; align-items: center;">
-        <p style="margin: 5px;"><strong>Idade: </strong> ${
-          solicitationDetails.pet.age
-        }</p>
-        <p style="margin: 5px;"><strong>Sexo do Animal: </strong> ${
-          solicitationDetails.pet.sex
-        }</p>
-      </div>
+      <table style="width: 100%;">
+        <tr>
+          <th style="text-align: left;"><p style="margin: 5px;">Dados do Pet:</p></th>
+        </tr>
+        <tr>
+          <td>    
+            <p style="margin: 0 5px;"><strong>Nome do Animal: </strong> ${
+              solicitationDetails.pet.name
+            }</p>
+           </td>
+          <td style="text-align: end;">
+            <p style="margin: 0 5px;"><strong>Espécie: </strong> ${
+              solicitationDetails.pet.name
+            }</p>
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <p style="margin: 0 5px;"><strong>Idade: </strong> ${
+              solicitationDetails.pet.age
+            }</p>
+          </td>
+          <td style="text-align: end;"> 
+            <p style="margin: 0 5px;"><strong>Sexo do Animal: </strong> ${
+              solicitationDetails.pet.sex
+            }</p>
+          </td>
+        </tr>
+      </table>
     </div>
     <br>
-    <div style="border: 2px solid black; padding: 20px;">
+    <div style="border: 1px solid gray; padding: 20px;">
       <p style="margin: 0; text-align: center; font-size: 22px;">
         <strong>Exames Solicitados</strong>
       </p>
@@ -59,13 +87,14 @@ export const HtmlFinalizedSolicitation = (
         .map(
           (exam: any) =>
             `
-              <p>
-                ${exam.Exams.name}
+              <p style="margin-bottom: 0;">
+                - ${exam.Exams.name}
                 <p style="margin: 0; padding: 0;"><strong style="margin: 0; padding: 0;">Preparo: </strong>${exam.Exams.preparing}</p>
               </p>
             `
         )
         .join(``)}
+        <br>
         <p>
           <strong>Observações:</strong> ${
             !solicitationDetails.observation
@@ -74,21 +103,41 @@ export const HtmlFinalizedSolicitation = (
           }
         </p>
     </div>
-    <div style="margin-top: 50px">
-    </div>
-    <br />
-    <br />
-    <br />
   </div>
-  <br>
-  <div style="position: absolute; bottom: 50px; width: 100%; display: flex; align-items: center; font-size: 14px; gap: 20px; justify-content: center;">
-    <img style="width: 100px;" src="https://www.oficinadanet.com.br/media/post/2322/330/qrcode.png" alt="">
+  <table style="font-size: 12px; width: 80%; margin: auto; margin-top: 50px">
+    <tr>
+      <tr>
+        <td rowspan="5">
+        <img style="width: 100px;" src="https://www.oficinadanet.com.br/media/post/2322/330/qrcode.png" alt="">
+        </td>
+      </tr>
+      <tr>
+        <td>
+          <p style="margin: 0;"><strong>RK Diagnóstico</strong> – Acesso WhatsApp via QR Code</p>
+        </td>
+      </tr>
+      <tr>
+        <td>
+            <p style="margin: 0;"><strong>Endereço:</strong> R. Ártico, 248 - Jardim do Mar, São Bernardo do Campo - SP, 09726-300</p>
+        </td>
+      </tr>
+      <tr>
+        <td>
+            <p style="margin: 0;"><strong>Telefone:</strong> (11) 4122-3733</p>
+        </td>
+      </tr>
+      <tr>
+        <td>
+            <p style="margin: 0;"><strong>Assinado digitalmente por:</strong> ${
+              solicitationDetails.veterinarians.name
+            } <strong>CRMV:</strong> ${
+    solicitationDetails.veterinarians.crmv
+  }</p>
+        </td>
+      </tr>
+    </tr>
     <div>
-    <p style="margin: 0;"><strong>RK Diagnóstico</strong> – Acesso WhatsApp via QR Code</p>
-    <p style="margin: 0;"><strong>Endereço:</strong> R. Ártico, 248 - Jardim do Mar, São Bernardo do Campo - SP, 09726-300</p>
-    <p style="margin: 0;"><strong>Telefone:</strong> (11) 4122-3733</p>
-    <p style="margin: 0;"><strong>Assinado digitalmente por:</strong> {solicitationDetails.veterinarians.name} <strong>CRMV:</strong> {solicitationDetails.veterinarians.crmv}</p>
     </div>
-  </div>
+  </table>
 </body>`;
 };
