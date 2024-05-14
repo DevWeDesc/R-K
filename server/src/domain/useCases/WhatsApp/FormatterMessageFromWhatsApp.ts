3fe75aaed2b8b2a3d20d84081935ab7292d49fb2
ViniHGV <ixxvinicius@gmail.com>
2024-05-14 08:50:50 -0300

    feat: criando formatador de mensagem para whatsapp

---
+++ server/src/domain/useCases/WhatsApp/FormatterMessageFromWhatsApp.ts
@@ -0,0 +1,43 @@
+import { FormatedDate } from "../../../utils/FormatedDate";
+import { SolicitationModel } from "../../models/Solicitation";
+
+export const FormatterMessageFromWhatsApp = (
+  solicitationModel: SolicitationModel,
+  pdfName: string
+): string => {
+  return `
+*Nome do Tutor*: ${solicitationModel.pet.customer.name}
+*Data e Hora*: ${FormatedDate(new Date(), "short", "medium")}
+*CPF*: ${solicitationModel.pet.customer.cpf}
+*E-mail*: ${solicitationModel.pet.customer.email}
+
+*Dados do Pet*
+*Nome*: ${solicitationModel.pet.name}        *Espécie*: ${
+    solicitationModel.pet.specie
+  }
+*Idade*: ${solicitationModel.pet.age}        *Sexo do animal*: ${
+    solicitationModel.pet.sex
+  }
+
+*Exames Solicitados*
+
+${solicitationModel.exams
+  .map(
+    (exam) =>
+      `
+*Exame:* ${exam.Exams.name}
+*Preparo*: ${exam.Exams.preparing}
+`
+  )
+  .join(``)}
+
+*Observações*: ${
+    !solicitationModel.observation
+      ? "Sem observações!"
+      : solicitationModel.observation
+  }
+
+*Guia gerada com sucesso na R&K para o pet ${solicitationModel.pet.name}!*
+Segue o link da Guia: *http://localhost:3333/solicitations/pdf/${pdfName}*
+`;
+};
