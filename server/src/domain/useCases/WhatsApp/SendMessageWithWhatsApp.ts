cafdbb34fce4a17e9394850b14e1c39df7c677ac
ViniHGV <ixxvinicius@gmail.com>
2024-05-14 08:51:18 -0300

    fix: Ajustando envio de menssagem

--- server/src/domain/useCases/WhatsApp/SendMessageWithWhatsApp.ts
+++ server/src/domain/useCases/WhatsApp/SendMessageWithWhatsApp.ts
@@ -1,17 +1,25 @@
-const client = require("twilio")(
-  process.env.ACCOUNTSIDWHATS,
-  process.env.AUTHTOKENWHATS
+const twilioClient = require("twilio")(
+  process.env.ACCOUNTSIDWHATS as string,
+  process.env.AUTHTOKENWHATS as string
 );
 
 export default class SendMessageWithWhatsApp {
-  public async execute(message: string, numberReceiver: string) {
-    await client.messages
+  public async execute(
+    message: string,
+    numberReceiver: string,
+    pathMedia?: string
+  ) {
+    numberReceiver = numberReceiver.replace(/[()\s]/g, "").replace(" ", "");
+
+    await twilioClient.messages
       .create({
+        mediaUrl: [pathMedia],
         body: message,
         from: "whatsapp:+14155238886",
         to: `whatsapp:${numberReceiver}`,
       })
-      .catch(() => {
+      .catch((err: any) => {
+        console.log(err);
         throw new Error("Erro ao enviar WhatsApp!");
       });
   }
