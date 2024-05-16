75bb161260af224223d3901a5f9e825e9e18f3ec
ViniHGV <ixxvinicius@gmail.com>
2024-05-16 13:00:30 -0300

    feat: criando schema para registro

---
+++ app/src/schemas/RegisterSchema.ts
@@ -0,0 +1,17 @@
+import { IRegisterForm } from "@/@interfaces/IRegisterForm";
+import { ZodType, z } from "zod";
+
+export const RegisterSchema: ZodType<IRegisterForm> = z.object({
+  name: z.string().min(1, { message: "O campo nome é obrigatório" }),
+  crmv: z
+    .string()
+    .min(5, { message: "CRMV inválido" })
+    .max(5, { message: "CRMV inválido" }),
+  email: z.string().email({ message: "E-mail informado é inválido!" }),
+  phone: z.string().min(1, { message: "O campo Telefone é obrigatório" }),
+  state: z.string().min(1, { message: "O campo Estado é obrigatório" }),
+  password: z.string().min(1, { message: "O campo Senha é obrigatório" }),
+  confirmationPassword: z
+    .string()
+    .min(1, { message: "O campo Confirmar Senha é obrigatório" }),
+});
