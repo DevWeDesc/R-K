1e6c76aaaa483ea0a0da496f3a0d996497b0df5d
ViniHGV <ixxvinicius@gmail.com>
2024-05-21 10:48:26 -0300

    feat: adicionando modelo default do PDF

--- server/src/domain/models/pdf/PdfDocument.tsx
+++ server/src/domain/models/pdf/PdfDocument.tsx
@@ -5,9 +5,11 @@ import ReactPDF, {
   View,
   Document,
   StyleSheet,
+  Image,
 } from "@react-pdf/renderer";
 import { SolicitationModel } from "../Solicitation";
 import path from "path";
+import { FormatedDate } from "../../../utils/FormatedDate";
 
 interface PDFProps {
   data: SolicitationModel;
@@ -15,23 +17,18 @@ interface PDFProps {
 
 const styles = StyleSheet.create({
   page: {
-    backgroundColor: "#E4E4E4",
+    backgroundColor: "#FFF",
   },
   section: {
     margin: 10,
     padding: 10,
   },
   heading: {
-    fontSize: 24,
-    fontWeight: 600,
-    color: "#131925",
-    marginBottom: 8,
-  },
-  statement: {
-    fontSize: 20,
-    color: "#131925",
-    lineHeight: 1.4,
-    marginBottom: 4,
+    marginBottom: 20,
+    fontSize: 16,
+    fontWeight: "bold",
+    color: "#000",
+    textAlign: "center",
   },
   divider: {
     width: "100%",
@@ -41,19 +38,28 @@ const styles = StyleSheet.create({
   },
   paragraph: {
     fontSize: 12,
-    color: "#212935",
+    color: "#2e2e2e",
     lineHeight: 1.67,
   },
-  columnParent: {
+  paragraphBold: {
+    fontWeight: 700,
+    fontSize: 12,
+    color: "#000",
+    lineHeight: 1.67,
+  },
+  paragraphSM: {
+    fontSize: 8,
+    color: "#000",
+    lineHeight: 1.2,
+  },
+  containerFather: {
     flexDirection: "row",
+    width: "100%",
     justifyContent: "space-between",
+    alignItems: "center",
   },
-  columnStart: {
-    flex: 1,
-  },
-  columnEnd: {
-    flex: 1,
-    alignItems: "flex-end",
+  containerChild: {
+    flexDirection: "row",
   },
 });
 
@@ -62,7 +68,123 @@ const PDF = ({ data }: PDFProps) => {
     <Document>
       <Page size="A4" style={styles.page}>
         <View style={styles.section}>
-          <Text style={styles.heading}>{data.pet.customer.name}</Text>
+          <Image
+            style={{ objectFit: "cover", width: "25%", margin: "0 auto" }}
+            source="https://rkdiagnostico.com.br/wp-content/uploads/2015/06/logo_RK_2015_2.png"
+          />
+          <View
+            style={{
+              border: "1px solid black",
+              padding: "20px",
+              marginTop: 30,
+            }}
+          >
+            <Text style={styles.heading}>Solicitação de Exame</Text>
+            <View style={styles.containerFather}>
+              <View style={styles.containerChild}>
+                <Text style={styles.paragraphBold}>Nome do Tutor: </Text>
+                <Text style={styles.paragraph}>{data.pet.customer.name}</Text>
+              </View>
+              <View style={styles.containerChild}>
+                <Text style={styles.paragraphBold}>Data e Hora: </Text>
+                <Text style={styles.paragraph}>
+                  {FormatedDate(
+                    new Date(data.finishedIn),
+                    "short",
+                    "medium"
+                  ).replace(",", " -")}
+                </Text>
+              </View>
+            </View>
+            <View style={styles.containerChild}>
+              <Text style={styles.paragraphBold}>CPF: </Text>
+              <Text style={styles.paragraph}>{data.pet.customer.cpf}</Text>
+            </View>
+            <View style={styles.containerChild}>
+              <Text style={styles.paragraphBold}>E-mail: </Text>
+              <Text style={styles.paragraph}>{data.pet.customer.email}</Text>
+            </View>
+            <View style={{ marginTop: 20 }}>
+              <Text style={styles.paragraphBold}>Dados do Pet: </Text>
+              <View style={styles.containerFather}>
+                <View style={styles.containerChild}>
+                  <Text style={styles.paragraphBold}>Nome do Animal: </Text>
+                  <Text style={styles.paragraph}>{data.pet.name}</Text>
+                </View>
+                <View style={styles.containerChild}>
+                  <Text style={styles.paragraphBold}>Espécie: </Text>
+                  <Text style={styles.paragraph}>{data.pet.specie}</Text>
+                </View>
+              </View>
+              <View style={styles.containerFather}>
+                <View style={styles.containerChild}>
+                  <Text style={styles.paragraphBold}>Idade: </Text>
+                  <Text style={styles.paragraph}>{data.pet.age}</Text>
+                </View>
+                <View style={styles.containerChild}>
+                  <Text style={styles.paragraphBold}>Sexo do Animal: </Text>
+                  <Text style={styles.paragraph}>{data.pet.sex}</Text>
+                </View>
+              </View>
+            </View>
+          </View>
+          <View
+            style={{
+              border: "1px solid black",
+              padding: "20px",
+              marginTop: 30,
+            }}
+          >
+            <View>
+              <Text style={styles.heading}>Exames Solicitados</Text>
+              {data.exams.map((exam) => (
+                <View style={{ marginTop: 10 }}>
+                  <Text style={styles.paragraph}>- {exam.Exams.name}</Text>
+                  <View style={styles.containerChild}>
+                    <Text style={styles.paragraphBold}>Preparo: </Text>
+                    <Text style={styles.paragraph}>{exam.Exams.preparing}</Text>
+                  </View>
+                </View>
+              ))}
+              <View style={{ marginTop: 10 }}>
+                <View style={styles.containerChild}>
+                  <Text style={styles.paragraphBold}>Observações: </Text>
+                  <Text style={styles.paragraph}>
+                    {!data.observation ? "Sem observações!" : data.observation}
+                  </Text>
+                </View>
+              </View>
+            </View>
+          </View>
+          <View style={{ marginTop: 30 }}>
+            <View
+              style={{
+                flexDirection: "row",
+                justifyContent: "center",
+                alignItems: "center",
+                gap: 10,
+              }}
+            >
+              <Image
+                style={{ width: "10%" }}
+                source="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSojqE7ztWUYLgAOLfIp8OrWRqDI-43JplTC8XdCZ8L9g&s"
+              />
+              <View style={{ height: "100%", justifyContent: "space-between" }}>
+                <Text style={styles.paragraphSM}>
+                  RK Diagnóstico - Acesso WhatsApp via QR Code
+                </Text>
+                <Text style={styles.paragraphSM}>
+                  Endereço: R. Ártico, 248 - Jardim do Mar, São Bernardo do
+                  Campo - SP, 09726-300
+                </Text>
+                <Text style={styles.paragraphSM}>Telefone: (11) 4122-3733</Text>
+                <Text style={styles.paragraphSM}>
+                  Assinado digitalmente por: {data.veterinarians.name} CRMV:{" "}
+                  {data.veterinarians.crmv}
+                </Text>
+              </View>
+            </View>
+          </View>
         </View>
       </Page>
     </Document>
