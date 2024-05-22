a0656d24a31fd4dc4f43a49937bacfa336b86bc5
ViniHGV <ixxvinicius@gmail.com>
2024-05-22 12:32:30 -0300

    fix: ajustando path para path relativo

--- server/src/domain/useCases/Solicitations/PDFPerSolicitation/PDFPerSolicitationUseCase.ts
+++ server/src/domain/useCases/Solicitations/PDFPerSolicitation/PDFPerSolicitationUseCase.ts
@@ -1,10 +1,15 @@
 const fs = require("fs");
+import path from "path";
 
 export default class PDFPerSolicitationUseCase {
   constructor() {}
 
   public async execute(namePdf: string) {
-    const pathFile = `./src/infra/PDFs/${namePdf}.pdf`;
+    const pathFile = path.join(
+      __dirname,
+      "../../../",
+      `src/infra/PDFs/${namePdf}.pdf`
+    );
 
     if (!fs.existsSync(pathFile)) throw new Error("Arquivo não encontrado");
 
