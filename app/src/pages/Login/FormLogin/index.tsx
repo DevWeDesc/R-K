52928bcfd1d7808d8e59b01061ec0f5986913807
ViniHGV <ixxvinicius@gmail.com>
2024-05-22 12:30:57 -0300

    feat: adicionando select no login com os estados brasileiros e modificando tipo do input do crmv

--- app/src/pages/Login/FormLogin/index.tsx
+++ app/src/pages/Login/FormLogin/index.tsx
@@ -1,15 +1,25 @@
 import { ILoginUser } from "@/@interfaces/ILoginForm";
 import { useState } from "react";
-import { useForm } from "react-hook-form";
+import { Controller, useForm } from "react-hook-form";
 import { useNavigate } from "react-router-dom";
 import Cookies from "js-cookie";
 import { toast } from "react-toastify";
-import { InputRequiredError } from "@/components/Errors/InputRequiredError";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { FaRegEye, FaRegEyeSlash } from "react-icons/fa6";
 import { LoginService } from "@/services/User/LoginService";
 import { ImSpinner8 } from "react-icons/im";
+import {
+  Select,
+  SelectTrigger,
+  SelectValue,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+} from "@/components/ui/select";
+import { StatesMock } from "@/mocks/StatesMock";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { LoginSchema } from "@/schemas/LoginSchema";
 
 export const FormLogin = () => {
   const navigate = useNavigate();
@@ -17,9 +27,10 @@ export const FormLogin = () => {
   const [buttonIsLoading, setButtonIsLoading] = useState(false);
   const {
     register,
+    control,
     handleSubmit,
     formState: { errors },
-  } = useForm<ILoginUser>();
+  } = useForm<ILoginUser>({ resolver: zodResolver(LoginSchema) });
 
   const loginSucces = (res: any) => {
     localStorage.user = JSON.stringify(res.data.tokenInformations);
@@ -27,19 +38,24 @@ export const FormLogin = () => {
     Cookies.set("userRole", `${res.data.tokenInformations.user.roleUser}`, {
       expires: 7,
     });
+    setButtonIsLoading(false);
     navigate("/home");
   };
 
   const onSubmit = handleSubmit(async (data) => {
+    let { state, crmv, email, password } = data;
     const handleSubmitUser = {
-      crmv: data.crmv,
-      email: data.email,
-      password: data.password,
+      state,
+      crmv,
+      email,
+      password,
     };
+    setButtonIsLoading(true);
 
     await LoginService(handleSubmitUser)
       .then((res) => loginSucces(res))
-      .catch(() => {
+      .catch((err) => {
+        console.log(err);
         toast.error("Erro ao realizar login! Verifique os dados fornecidos!");
         setButtonIsLoading(false);
       });
@@ -52,29 +68,63 @@ export const FormLogin = () => {
   return (
     <form onSubmit={onSubmit} className="flex flex-col gap-4 w-full">
       <div className="flex flex-col gap-3">
-        <div>
+        <div className="grid grid-cols-2 gap-1">
+          <Controller
+            name="state"
+            control={control}
+            defaultValue="SP"
+            render={({ field }) => (
+              <>
+                <Select onValueChange={field.onChange} defaultValue="SP">
+                  <SelectTrigger className="w-full bg-zinc-200 py-6 rounded-full">
+                    <SelectValue
+                      defaultValue="SP"
+                      placeholder="São Paulo"
+                      {...field}
+                    />
+                  </SelectTrigger>
+                  <SelectContent>
+                    <SelectGroup>
+                      {StatesMock.map((state) => (
+                        <SelectItem key={state.acronym} value={state.acronym}>
+                          {state.state}
+                        </SelectItem>
+                      ))}
+                    </SelectGroup>
+                  </SelectContent>
+                </Select>
+                {errors.state && <p>{errors.state.message}</p>}
+              </>
+            )}
+          />
           <Input
-            {...register("crmv", { required: true })}
-            placeholder="Sigla do estado + Nº CRMV (SP-123)"
+            {...register("crmv")}
+            type="number"
+            placeholder="Numeração do CRMV"
           />
           {errors.crmv && (
-            <InputRequiredError className="px-4" inputName="CRMV" />
+            <div className="col-start-2">
+              {errors.crmv && (
+                <p className="text-red-700 text-xs pl-2 font-medium">
+                  {errors.crmv.message}
+                </p>
+              )}
+            </div>
           )}
         </div>
         <div>
-          <Input
-            {...register("email", { required: true })}
-            placeholder="E-mail"
-          />
+          <Input {...register("email")} placeholder="E-mail" />
           {errors.email && (
-            <InputRequiredError className="px-4" inputName="E-mail" />
+            <p className="text-red-700 text-xs pl-2 font-medium">
+              {errors.email.message}
+            </p>
           )}
         </div>
         <div>
           <div className="relative">
             <Input
               type={!InputPasswordIsVisible ? "password" : "text"}
-              {...register("password", { required: true })}
+              {...register("password")}
               placeholder="Senha"
             />
             {InputPasswordIsVisible ? (
@@ -97,8 +147,11 @@ export const FormLogin = () => {
               </Button>
             )}
           </div>
+
           {errors.password && (
-            <InputRequiredError className="px-4" inputName="Senha" />
+            <p className="text-red-700 text-xs pl-2 font-medium">
+              {errors.password.message}
+            </p>
           )}
         </div>
       </div>
