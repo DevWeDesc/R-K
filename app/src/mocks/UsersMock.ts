30cab261666fccbeae26928382de02e9aa7cf00e
ViniHGV <ixxvinicius@gmail.com>
2024-05-23 10:22:04 -0300

    fix: ajustando mock

--- app/src/mocks/UsersMock.ts
+++ app/src/mocks/UsersMock.ts
@@ -7,11 +7,13 @@ export const UsersMock: ILoginUser[] = [
     email: "daniel@gmail.com",
     password: "123",
     userRole: userRoleEnum.admin,
+    state: "SP - São Paulo",
   },
   {
     crmv: "12345",
     email: "teste@gmail.com",
     password: "teste123",
     userRole: userRoleEnum.veterinarian,
+    state: "SP - São Paulo",
   },
 ];
