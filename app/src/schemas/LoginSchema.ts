240bcbb6195a1569598f0ca6625ee89beb5d666d
ViniHGV <ixxvinicius@gmail.com>
2024-05-23 10:24:19 -0300

    feat: adicionando verificação de e-mail no schema

--- app/src/schemas/LoginSchema.ts
+++ app/src/schemas/LoginSchema.ts
@@ -4,6 +4,9 @@ import { ZodType, z } from "zod";
 export const LoginSchema: ZodType<ILoginUser> = z.object({
   state: z.string().min(1, { message: "O campo senha é obrigatório!" }),
   crmv: z.string().length(5, { message: "Numeração de CRMV inválida" }),
-  email: z.string().min(1, { message: "O campo senha é obrigatório!" }),
+  email: z
+    .string()
+    .email({ message: "O E-mail informado é inválido!" })
+    .min(1, { message: "O campo senha é obrigatório!" }),
   password: z.string().min(1, { message: "O campo senha é obrigatório!" }),
 });
