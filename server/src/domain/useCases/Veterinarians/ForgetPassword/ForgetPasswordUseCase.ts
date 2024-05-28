60589b287255c8459178146ba3f6fd89dc4d1f11
ViniHGV <ixxvinicius@gmail.com>
2024-05-28 09:48:28 -0300

    feat: criando getters e setters para email e código e realizando ajustes

--- server/src/domain/useCases/Veterinarians/ForgetPassword/ForgetPasswordUseCase.ts
+++ server/src/domain/useCases/Veterinarians/ForgetPassword/ForgetPasswordUseCase.ts
@@ -1,12 +1,12 @@
-import { VeterinarianRepository } from "../../../infra/repositories/Veterinarian/VeterinarianRepository";
-import { EmailValidator } from "../../../utils/ValidateEmail";
-import EmailNotValidError from "../../errors/Customers/EmailNotValidError";
-import { GenerateCode } from "../../../utils/GenerateCode";
-import { sendMailUseCase } from "../../..";
-import SendMailUseCase from "../Mail/SendMailUseCase";
-import { ReceiverMailDTO } from "../../../application/DTOs/MailDTO/ReceiverMailDTO";
+import { sendMailUseCase } from "../../../..";
+import { ReceiverMailDTO } from "../../../../application/DTOs/MailDTO/ReceiverMailDTO";
+import { VeterinarianRepository } from "../../../../infra/repositories/Veterinarian/VeterinarianRepository";
+import { GenerateCode } from "../../../../utils/GenerateCode";
+import { EmailValidator } from "../../../../utils/ValidateEmail";
+import EmailNotValidError from "../../../errors/Customers/EmailNotValidError";
+import SendMailUseCase from "../../Mail/SendMailUseCase";
 
-interface IForgetPasswordUseCase {
+export interface IForgetPasswordUseCase {
   execute(email: string): Promise<string>;
 }
 
@@ -16,10 +16,21 @@ export class ForgetPasswordUseCase implements IForgetPasswordUseCase {
     readonly sendMailUseCase: SendMailUseCase
   ) {}
 
-  private code: string = GenerateCode();
+  private code: string = "";
+  private email: string = "";
 
   public getCode() {
     return this.code;
+  }
+  public getEmail() {
+    return this.email;
+  }
+
+  private setCode(code: string) {
+    this.code = code;
+  }
+  private setEmail(email: string) {
+    this.email = email;
   }
 
   async execute(email: string): Promise<string> {
@@ -29,14 +40,15 @@ export class ForgetPasswordUseCase implements IForgetPasswordUseCase {
 
     if (!userByEmail) throw new Error("E-mail informado não existe!");
 
+    this.setCode(GenerateCode());
+    this.setEmail(userByEmail.email);
+
     const receiver: ReceiverMailDTO = {
       subject: "Código de verificação de E-mail",
       html: `<h3>Olá ${userByEmail.name} seu código de verificação é: <strong>${this.code}</strong></h3>`,
       to: userByEmail.email,
       contentFile: "",
     };
-
-    this.code = GenerateCode();
 
     await sendMailUseCase.execute(receiver);
 
