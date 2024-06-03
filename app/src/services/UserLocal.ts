1d0442052abc8e2dece95471cdce069e3bc0a4dd
ViniHGV <ixxvinicius@gmail.com>
2024-06-03 12:16:29 -0300

    fix: ajustando Authorization

--- app/src/services/UserLocal.ts
+++ app/src/services/UserLocal.ts
@@ -1,6 +1,6 @@
 const userLoged = localStorage.user ? JSON.parse(localStorage?.user) : null;
 export const headerRequest = {
   headers: {
-    Authorization: userLoged?.token,
+    Authorization: `Bearer ${userLoged?.token}`,
   },
 };
