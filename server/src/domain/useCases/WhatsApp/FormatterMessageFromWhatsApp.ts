d399281c3c54c2fd9e4f29e3c7891622d9f068ca
ViniHGV <ixxvinicius@gmail.com>
2024-06-03 12:21:24 -0300

    feat: adicionando telefone do cliente e link para o whastapp do cliente

--- server/src/domain/useCases/WhatsApp/FormatterMessageFromWhatsApp.ts
+++ server/src/domain/useCases/WhatsApp/FormatterMessageFromWhatsApp.ts
@@ -1,14 +1,17 @@
 import { FormatedDate } from "../../../utils/FormatedDate";
+import { FormatterPhone } from "../../../utils/FormatterPhone";
 import { SolicitationModel } from "../../models/Solicitation";
 
 export const FormatterMessageFromWhatsApp = (
   solicitationModel: SolicitationModel,
   pdfName: string
 ): string => {
+  const numberFormatted = FormatterPhone(solicitationModel.pet.customer.phone);
   return `
 *Nome do Tutor*: ${solicitationModel.pet.customer.name}
 *Data e Hora*: ${FormatedDate(new Date(), "short", "medium")}
 *E-mail*: ${solicitationModel.pet.customer.email}
+*Telefone*: ${solicitationModel.pet.customer.phone}
 
 *Dados do Pet*
 *Nome*: ${solicitationModel.pet.name}        *Espécie*: ${
@@ -38,5 +41,10 @@ ${solicitationModel.exams
 
 *Guia gerada com sucesso na R&K para o pet ${solicitationModel.pet.name}!*
 Segue o link da Guia: *https://rkdiagnostico.cloud/solicitations/pdf/${pdfName}*
+
+*Link para o WhatsApp do cliente:* https://api.whatsapp.com/send?phone=55${numberFormatted}&text=Olá%20${solicitationModel.pet.customer.name.replace(" ", "%20")
+}%20aqui%20é%20a%20RK!%20Segue%20o%20link%20da%20guia%20do%20${
+solicitationModel.pet.name
+}:%20*https://rkdiagnostico.cloud/solicitations/pdf/${pdfName}*
 `;
 };
