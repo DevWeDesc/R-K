db300b590313189f7fee1cc1f16251fcf9f38a9d
ViniHGV <ixxvinicius@gmail.com>
2024-06-10 09:54:42 -0300

    feat: consumindo serviço de relatórios por vet e exibindo retorno

--- app/src/pages/Reports/index.tsx
+++ app/src/pages/Reports/index.tsx
@@ -1,7 +1,15 @@
 import { Header } from "@/components/Header";
 import { Graphic } from "./Graphics";
+import { useQuery } from "react-query";
+import { SolicitationsPerVet } from "@/services/Solicitations/SolicitationsPerVet";
+import { ImSpinner8 } from "react-icons/im";
 
 export const Reports = () => {
+  const { data: reports, isLoading } = useQuery({
+    queryKey: ["reports"],
+    queryFn: async () => SolicitationsPerVet(),
+  });
+
   return (
     <>
       <Header navIsVisible />
@@ -9,16 +17,46 @@ export const Reports = () => {
         <p className="text-center font-semibold text-4xl">
           Relatório de exames <br /> solicitados por Veterinários
         </p>
-        <div className="grid grid-cols-2 gap-y-10">
-          <Graphic typeChart="pie" />
-          <Graphic typeChart="donut" />
-          <div className="w-full col-span-2">
-            <Graphic
-              typeChart="bar"
-              dataDescriptions={["Daniel", "Felipe", "Adriano"]}
-            />
+        {isLoading ? (
+          <div className="h-[30vh] w-full flex items-center justify-center">
+            <ImSpinner8 className="animate-spin text-9xl" />
           </div>
-        </div>
+        ) : (
+          <div className="grid grid-cols-2 gap-y-10">
+            <Graphic
+              seriesData={reports?.data.map(
+                (report) => report._count.solicitations
+              )}
+              dataDescriptions={reports?.data.map(
+                (report) =>
+                  `${report.name} - Qtd Exames: ${report._count.solicitations}`
+              )}
+              typeChart="pie"
+            />
+            <Graphic
+              seriesData={reports?.data.map(
+                (report) => report._count.solicitations
+              )}
+              dataDescriptions={reports?.data.map(
+                (report) =>
+                  `${report.name} - Qtd Exames: ${report._count.solicitations}`
+              )}
+              typeChart="donut"
+            />
+            <div className="w-full col-span-2">
+              <Graphic
+                seriesData={reports?.data.map(
+                  (report) => report._count.solicitations
+                )}
+                typeChart="bar"
+                dataDescriptions={reports?.data.map(
+                  (report) =>
+                    `${report.name} - Qtd Exames: ${report._count.solicitations}`
+                )}
+              />
+            </div>
+          </div>
+        )}
       </div>
     </>
   );
