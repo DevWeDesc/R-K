e2f0e1c59373c98ecb82c62c47ece07cca682598
ViniHGV <ixxvinicius@gmail.com>
2024-06-13 13:00:32 -0300

    feat: adicionando parâmetros no método getSolicitationsByVeterinarian

--- server/src/infra/repositories/Veterinarian/IVeterinarianRepository.ts
+++ server/src/infra/repositories/Veterinarian/IVeterinarianRepository.ts
@@ -1,11 +1,16 @@
 import { Veterinarians } from "@prisma/client";
-import { IVeterinarianModel } from "../../../domain/models/Veterinarian";
 import { IGenericRepository } from "../IGenericRepository";
 import VeterinarianRequestDTO from "../../../application/DTOs/VeterinarianDTO/VeterinarianRequestDTO";
+import { IVeterinarianModel } from "../../../domain/models/Veterinarian copy";
+import { SolicitationByVeterinarian } from "../../../domain/models/SolicitationByVeterinarian";
 
 export interface IVeterinarianRepository
   extends IGenericRepository<VeterinarianRequestDTO, Veterinarians> {
   getByEmail(email: string): Promise<IVeterinarianModel | null>;
   getByCRMV(crmv: string): Promise<IVeterinarianModel | null>;
-  getSolicitationsByVeterinarian(): Promise<IVeterinarianModel[]>;
+  getSolicitationsByVeterinarian(
+    initialDate?: string,
+    finalDate?: string,
+    name?: string
+  ): Promise<SolicitationByVeterinarian[]>;
 }
