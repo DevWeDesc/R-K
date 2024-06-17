05a3efc9358db6f186f1fa6d126d602a6f2ba757
ViniHGV <ixxvinicius@gmail.com>
2024-06-17 08:32:16 -0300

    feat: criando serviço para solicitações por veterinário, com queryString

--- app/src/services/Solicitations/SolicitationsPerVet.ts
+++ app/src/services/Solicitations/SolicitationsPerVet.ts
@@ -3,8 +3,25 @@ import { AxiosResponse } from "axios";
 import { headerRequest } from "../UserLocal";
 import { IReportPerVet } from "@/@interfaces/ReportsPerVet";
 
-export const SolicitationsPerVet = async (): Promise<
-  AxiosResponse<IReportPerVet[]>
-> => {
-  return await api.get("/veterinarian/solicitations", headerRequest);
+export interface ParamsSolicitationsPerVet {
+  initialDate?: string | null;
+  finalDate?: string | null;
+  name?: string | null;
+}
+
+export const SolicitationsPerVet = async (
+  paramsSolicitationsPerVet: ParamsSolicitationsPerVet
+): Promise<AxiosResponse<IReportPerVet[]>> => {
+  return await api.get(
+    `/veterinarian/solicitations?${
+      paramsSolicitationsPerVet.name && `name=${paramsSolicitationsPerVet.name}`
+    }${
+      paramsSolicitationsPerVet.initialDate &&
+      `&initialDate=${paramsSolicitationsPerVet.initialDate}`
+    }${
+      paramsSolicitationsPerVet.initialDate &&
+      `&finalDate=${paramsSolicitationsPerVet.finalDate}`
+    }`,
+    headerRequest
+  );
 };
