809426f5a8620f435fe9584d6cf4143faea34c3b
ViniHGV <ixxvinicius@gmail.com>
2024-06-20 13:26:19 -0300

    feat: ajustando queryParams do serviço de getExams

--- app/src/services/Exams/GetExams.ts
+++ app/src/services/Exams/GetExams.ts
@@ -4,12 +4,13 @@ import { headerRequest } from "../UserLocal";
 
 export const GetExams = async (
   pageActual?: number,
-  examName?: string
+  examName?: string,
+  filterOptions?: string
 ): Promise<AxiosResponse> => {
   return await api.get(
-    `/exams?${pageActual != undefined && `pageActual=${pageActual}`}&${
-      examName && `name=${examName}`
-    }`,
+    `/exams?${pageActual != undefined && `pageActual=${pageActual}`}${
+      examName ? `&name=${examName}` : ""
+    }${filterOptions ? `&filterByValue=${filterOptions}` : ""}`,
     headerRequest
   );
 };
