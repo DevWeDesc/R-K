20ce4dc8ac1652a214614e1f4e0b5f9a53f72dc8
ViniHGV <ixxvinicius@gmail.com>
2024-06-20 13:29:00 -0300

    feat: adicionando campos no dto

--- server/src/application/DTOs/ExamsDTO/ExamsRequestDTO.ts
+++ server/src/application/DTOs/ExamsDTO/ExamsRequestDTO.ts
@@ -2,6 +2,9 @@ export interface ExamsRequestDTO {
   id?: number;
   name: string;
   value: number;
+  valueWithRate?: number;
+  isHighligth?: boolean;
+  specie?: string;
   groupId?: number;
   deadline: string;
   preparing?: string;
