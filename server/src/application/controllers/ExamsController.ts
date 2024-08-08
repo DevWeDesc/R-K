e5ed96b723728c2e824cc7f33775590bf0c1dee5
ViniHGV <ixxvinicius@gmail.com>
2024-08-08 12:22:38 -0300

    feat: adicionando novos endpoints de exames

--- server/src/application/controllers/ExamsController.ts
+++ server/src/application/controllers/ExamsController.ts
@@ -1,12 +1,25 @@
 import { FastifyRequest, FastifyReply } from "fastify";
 import {
+  createExamsInExamProfileUseCase,
+  createExamsProfileUseCase,
   createExamsUseCase,
+  deleteExamInExamProfileUseCase,
+  deleteExamProfileUseCase,
   deleteExamUseCase,
+  getExamsInExamProfileUseCase,
+  getExamsPerTypeUseCaseImpl,
+  getExamsProfileUseCase,
   getExamsUseCase,
+  updateExamInExamProfileUseCase,
+  updateExamProfileUseCase,
   updateExamUseCase,
 } from "../..";
 import { ExamsRequestDTO } from "../DTOs/ExamsDTO/ExamsRequestDTO";
 import { QueryParamsListAllExams } from "../DTOs/ExamsDTO/QueryParamsListAllExams";
+import { ExamTypeEnum } from "../../domain/enums/ExamTypeEnum";
+import GetExamsPerTypeUseCase from "../../domain/useCases/Exams/GetExamsPerTypeUseCase";
+import { ExamsProfileRequestDTO } from "../DTOs/ExamsProfileDTO/ExamsProfileRequestDTO";
+import { ExamsInExamProfileRequestDTO } from "../DTOs/ExamsInExamProfile/ExamsInExamProfileRequestDTO";
 
 export const ExamsController = {
   GetExams: async (
@@ -17,6 +30,23 @@ export const ExamsController = {
   ) => {
     try {
       const res = await getExamsUseCase.execute(request.query);
+
+      return reply.code(200).send(res);
+    } catch (err) {
+      return reply.code(400).send(err);
+    }
+  },
+
+  GetExamsPerType: async (
+    request: FastifyRequest<{
+      Querystring: { typeExam: ExamTypeEnum };
+    }>,
+    reply: FastifyReply
+  ) => {
+    try {
+      const res = await getExamsPerTypeUseCaseImpl.execute(
+        request.query.typeExam
+      );
 
       return reply.code(200).send(res);
     } catch (err) {
@@ -68,6 +98,127 @@ export const ExamsController = {
       await updateExamUseCase.execute(parseInt(id), body);
 
       return reply.code(204).send();
+    } catch (err) {
+      return reply.code(400).send(err);
+    }
+  },
+
+  GetExamsProfile: async (request: FastifyRequest, reply: FastifyReply) => {
+    try {
+      const res = await getExamsProfileUseCase.execute();
+
+      return reply.code(200).send(res);
+    } catch (err) {
+      return reply.code(400).send(err);
+    }
+  },
+
+  CreateExamsProfile: async (
+    request: FastifyRequest<{ Body: ExamsProfileRequestDTO }>,
+    reply: FastifyReply
+  ) => {
+    try {
+      const res = await createExamsProfileUseCase.execute(request.body);
+
+      return reply.code(201).send(res);
+    } catch (err) {
+      return reply.code(400).send(err);
+    }
+  },
+
+  UpdateExamsProfile: async (
+    request: FastifyRequest<{
+      Body: ExamsProfileRequestDTO;
+      Params: { id: string };
+    }>,
+    reply: FastifyReply
+  ) => {
+    try {
+      const { id } = request.params;
+      const res = await updateExamProfileUseCase.execute(
+        parseInt(id),
+        request.body
+      );
+
+      return reply.code(204).send(res);
+    } catch (err) {
+      return reply.code(400).send(err);
+    }
+  },
+
+  DeleteExamsProfile: async (
+    request: FastifyRequest<{
+      Params: { id: string };
+    }>,
+    reply: FastifyReply
+  ) => {
+    try {
+      const { id } = request.params;
+      const res = await deleteExamProfileUseCase.execute(parseInt(id));
+
+      return reply.code(204).send(res);
+    } catch (err) {
+      return reply.code(400).send(err);
+    }
+  },
+
+  GetExamsInExamProfile: async (
+    request: FastifyRequest,
+    reply: FastifyReply
+  ) => {
+    try {
+      const res = await getExamsInExamProfileUseCase.execute();
+
+      return reply.code(200).send(res);
+    } catch (err) {
+      return reply.code(400).send(err);
+    }
+  },
+
+  CreateExamsInExamProfile: async (
+    request: FastifyRequest<{ Body: ExamsInExamProfileRequestDTO }>,
+    reply: FastifyReply
+  ) => {
+    try {
+      const res = await createExamsInExamProfileUseCase.execute(request.body);
+
+      return reply.code(201).send(res);
+    } catch (err) {
+      return reply.code(400).send(err);
+    }
+  },
+
+  UpdateExamsInExamProfile: async (
+    request: FastifyRequest<{
+      Body: ExamsInExamProfileRequestDTO;
+      Params: { id: string };
+    }>,
+    reply: FastifyReply
+  ) => {
+    try {
+      const { id } = request.params;
+      const res = await updateExamInExamProfileUseCase.execute(
+        parseInt(id),
+        request.body
+      );
+
+      return reply.code(204).send(res);
+    } catch (err) {
+      return reply.code(400).send(err);
+    }
+  },
+
+  DeleteExamsInExamProfile: async (
+    request: FastifyRequest<{
+      Params: { id: string };
+    }>,
+    reply: FastifyReply
+  ) => {
+    try {
+      const { id } = request.params;
+      const res = await deleteExamInExamProfileUseCase.execute(parseInt(id));
+
+      return reply.code(204).send(res);
     } catch (err) {
       return reply.code(400).send(err);
     }
