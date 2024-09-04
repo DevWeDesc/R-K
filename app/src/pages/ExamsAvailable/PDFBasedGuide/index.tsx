f8075b106554dd04e6941038be221f86b58fa68d
ViniHGV <ixxvinicius@gmail.com>
2024-09-04 12:51:23 -0300

    fix: realizando ajustes do react hook form

--- app/src/pages/ExamsAvailable/PDFBasedGuide/index.tsx
+++ app/src/pages/ExamsAvailable/PDFBasedGuide/index.tsx
@@ -1,15 +1,21 @@
+import { useForm, FormProvider } from "react-hook-form";
 import { AllExamsWithType } from "./AllExamsWithType";
 import { CombinedExams } from "./CombinedExams/CombinedExams";
 import { HeaderGuide } from "./HeaderGuide";
 import { InformationsGuide } from "./InformationsGuide";
+import { IFormSolicitation } from "@/@interfaces/IFormSolicitation";
 
 export const PDFBasedGuide = () => {
+  const methods = useForm<IFormSolicitation>();
+
   return (
     <div className="w-full p-10">
       <HeaderGuide />
       <InformationsGuide />
-      <CombinedExams />
-      <AllExamsWithType />
+      <FormProvider {...methods}>
+        <CombinedExams />
+        <AllExamsWithType />
+      </FormProvider>
     </div>
   );
 };
