13585df09051ed042149d4e205b3451c241b429d
ViniHGV <ixxvinicius@gmail.com>
2024-09-05 13:09:26 -0300

    fix: ajustando dto para consumo do serviço

--- app/src/services/ExamsInPetOnSolicitations/CreateExamsInPetSolicitations.ts
+++ app/src/services/ExamsInPetOnSolicitations/CreateExamsInPetSolicitations.ts
@@ -1,10 +1,11 @@
-import CreateExamsInPetRequestDTO from "@/@interfaces/DTOs/ExamsInPetSolicitation/CreateExamsInPetRequestDTO";
+// import CreateExamsInPetRequestDTO from "@/@interfaces/DTOs/ExamsInPetSolicitation/CreateExamsInPetRequestDTO";
 import { api } from "@/utils/Api/AxiosInstance";
 import { AxiosResponse } from "axios";
 import { headerRequest } from "../UserLocal";
+import { ExamsInPetOnSolicitationsRequestDTO } from "@/@interfaces/DTOs/ExamsInPetSolicitation/ExamsInPetOnSolicitationsRequestDTO";
 
 export const CreateExamsInPetSolicitations = async (
-  createExamsInPetRequestDTO: CreateExamsInPetRequestDTO
+  createExamsInPetRequestDTO: ExamsInPetOnSolicitationsRequestDTO
 ): Promise<AxiosResponse> => {
   return await api.post(
     "/examsinpet/solicitations",
