83a61b039bc7c3bd961507d72f9e949e1ea20f91
ViniHGV <ixxvinicius@gmail.com>
2024-09-10 12:21:29 -0300

    feat: criando repositório para perfis de exames na guia

---
+++ server/src/infra/repositories/ExamsProfileInSolicitation/IExamsProfileInSolicitationRepository.ts
@@ -0,0 +1,15 @@
+import { ExamsProfileInSolicitation } from "@prisma/client";
+import { IGenericRepository } from "../IGenericRepository";
+import { CreateExamsProfileInSolicitationRequestDTO } from "../../../application/DTOs/ExamsProfileInSolicitation/CreateManyExamsProfileInSolicitationRequestDTO";
+import { CreateManyExamsProfileInSolicitationRequestDTO } from "../../../application/DTOs/ExamsProfileInSolicitation/CreateExamsProfileInSolicitationRequestDTO";
+import { CreateManyExamsProfileInSolicitationResponseDTO } from "../../../application/DTOs/ExamsProfileInSolicitation/CreateManyExamsProfileInSolicitationResponseDTO";
+
+export interface IExamsProfileInSolicitationRepository
+  extends IGenericRepository<
+    CreateExamsProfileInSolicitationRequestDTO,
+    ExamsProfileInSolicitation
+  > {
+  findByExamProfileId(
+    examProfileId: string
+  ): Promise<ExamsProfileInSolicitation[]>;
+}
