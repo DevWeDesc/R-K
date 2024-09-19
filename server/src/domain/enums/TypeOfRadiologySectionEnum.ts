05055e7739c43e2f63f4f65a007a3ff3f0919ab8
ViniHGV <ixxvinicius@gmail.com>
2024-09-19 11:53:09 -0300

    feat: criando enum de tipos de sessões de radiologia

---
+++ server/src/domain/enums/TypeOfRadiologySectionEnum.ts
@@ -0,0 +1,11 @@
+export enum TypeOfRadiologySectionEnum {
+  Skull = "Skull",
+  Skull_Dental_Arch = "Skull_Dental_Arch",
+  Members = "Members",
+  Pelvic_Limb = "Pelvic_Limb",
+  Abdomen = "Abdomen",
+  Chest = "Chest",
+  Spine = "Spine",
+  Projections = "Projections",
+  Cervical_Region = "Cervical_Region",
+}
