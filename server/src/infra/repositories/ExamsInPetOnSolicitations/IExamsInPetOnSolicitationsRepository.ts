a07ecba7431cb63c04eb0297f64b538381d03957
ViniHGV <ixxvinicius@gmail.com>
2024-10-08 13:00:21 -0300

    feat: criando métodos: createExamsWithMaterial e createExamsWithSample responsáveis pela criação de exames com meteriais e criação de exames com amostras

--- server/src/infra/repositories/ExamsInPetOnSolicitations/IExamsInPetOnSolicitationsRepository.ts
+++ server/src/infra/repositories/ExamsInPetOnSolicitations/IExamsInPetOnSolicitationsRepository.ts
@@ -1,6 +1,10 @@
 import { ExamsInPetOnSolicitations } from "@prisma/client";
 import { IGenericRepository } from "../IGenericRepository";
 import { ExamsInPetOnSolicitationsDTO } from "../../../application/DTOs/ExamsInPetOnSolicitationsDTO/ExamsInPetOnSolicitationsDTO";
+import {
+  IExamsWithMaterialInSolicitation,
+  IExamsWithSamples,
+} from "../../../application/DTOs/ExamsInPetOnSolicitationsDTO/ExamsInPetOnSolicitationsRequestDTO";
 
 export default interface IExamsInPetOnSolicitationsRepository
   extends IGenericRepository<
@@ -8,4 +12,12 @@ export default interface IExamsInPetOnSolicitationsRepository
     ExamsInPetOnSolicitations
   > {
   createMany(solicitationId: string, examsId: number[]): Promise<any>;
+  createExamsWithMaterial(
+    solicitationId: string,
+    entity: IExamsWithMaterialInSolicitation
+  ): Promise<any>;
+  createExamsWithSample(
+    solicitationId: string,
+    entity: IExamsWithSamples
+  ): Promise<any>;
 }
